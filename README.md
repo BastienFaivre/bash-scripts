d2c6e8d914f2f7c1910a6909ea62d4b46c11a5db
BastienFaivre <faivre.bast@gmail.com>
2022-05-20 20:24:19 +0200

    feat: first version of setup script

--- README.md
+++ README.md
@@ -1,6 +1,27 @@
 # debian-tools
 This repository aims to provide multiple useful tools (mainly scripts) for debian and debian-based machines.
-# Tools
-- to be completed
-# Contribution
+## Tools
+- Scripts
+  - to be completed
+## Setup and use the scripts
+To be able to use the scripts, simply give execution permission to the [setup script](scripts/setup.sh) located in the scripts folder:
+```
+sudo chmod u+x setup.sh
+```
+Then run the script:
+```
+./setup.sh
+```
+It will install the required packages, give all scripts execution permission, and finally create soft links into `/usr/local/bin`.
+
+To execute a script `foo.sh`, just type its name in the terminal (without the extension):
+```
+foo
+```
+To remove all the scripts from `/usr/local/bin`, simply run the [clean script](scripts/clean.sh) also located in the scripts folder:
+```
+./clean.sh
+```
+This will remove all the soft links created in `/usr/local/bin` by the setup script. Note that it won't delete your personal scripts, so no worries!
+## Contribution
 If you've any improvement and/or tool suggestions, feel free to fork the project, implement your ideas, and make a pull request in this repository.
---
+++ scripts/setup.sh
@@ -0,0 +1,59 @@
+#!/bin/bash
+
+execute_command() {
+    # check if a command is provided
+    if [ -z "$1" ]; then
+        echo "Execute: No command provided"
+        exit 1
+    fi
+    # execute the command in background
+    $1 > /dev/null & 
+    # display loader
+    pid=$!
+    i=1
+    sp="/-\|"
+    echo -n ' '
+    trap "kill $pid 2 > /dev/null" EXIT
+    while kill -0 $pid 2> /dev/null; do
+        # display spinner
+        printf "\b${sp:i++%${#sp}:1}"
+        sleep 0.1
+    done
+    printf "\b"
+    trap - EXIT
+}
+
+install_prerequisites() {
+    sudo apt-get -y install < ./requirements.txt
+}
+
+give_permission() {
+    sudo find . -name "*.sh" -exec chmod +x {} \;
+}
+
+create_softlinks() {
+    sudo find . ! -name "setup.sh" -name "*.sh" -exec cp {} /usr/local/bin \;
+    # removing file extension from scripts
+    for script in /usr/local/bin/*.sh; do
+        sudo mv $script /usr/local/bin/$(basename $script .sh)
+    done
+}
+
+echo -e "debian-tools setup\n"
+# ask for super user
+echo "Password required..."
+sudo echo -e "Password given!\n"
+# install prerequisites
+echo "Installing prerequisites..."
+execute_command "install_prerequisites"
+echo -e "Prerequisites installed!\n"
+# Give execute permissions to the scripts
+echo "Giving execution permission to scripts..."
+execute_command "give_permission"
+echo -e "Permission given!\n"
+# Copy scripts to /usr/local/bin which is usually already in the PATH
+echo "Creating soft links in /usr/local/bin..."
+execute_command "create_softlinks"
+echo -e "Soft links created!\n"
+# Done
+echo "Setup finished!"
