4cafe46955dfe3b199961aa20e6004a281cd864d
BastienFaivre <faivre.bast@gmail.com>
2022-06-01 17:58:39 +0200

    feat: issue #1

--- setup.sh
+++ setup.sh
@@ -5,6 +5,13 @@
 # import utility functions
 . ./utils/utils.sh
 
+usage() {
+  echo "Usage: ./setup.sh [OPTIONS]"
+  echo "OPTIONS:"
+  echo -e "\t-h, --help\t\t\tDisplay this help message"
+  echo -e "\t-t, --target-directory\t\tTarget directory to install the scripts"
+}
+
 install_prerequisites() {
   sudo cat ./requirements/apt.txt | xargs sudo apt-get install -y
 }
@@ -16,11 +23,25 @@ give_permission() {
 }
 
 create_softlinks() {
+  # create target directory if it doesn't exist
+  mkdir -p "${target}"
   # create softlinks without file extension
   for script in $(find ./scripts -name "*.sh"); do
-    sudo ln -s "$(realpath "${script}")" /usr/local/bin/"$(basename "${script}" .sh)"
+    sudo ln -s "$(realpath "${script}")" ""${target}"/$(basename "${script}" .sh)"
   done
 }
+
+target='/usr/local/bin'
+
+# retrieve the target directory if specified
+while [[ "$#" -gt 0 ]]; do 
+  case ${1} in
+    -h|--help) usage; exit 0;;
+    -t|--target-directory) target="${2}"; shift;;
+    *) echo "Unknown parameter passed: ${1}"; usage; exit 1;;
+  esac;
+  shift;
+done
 
 echo 'bash-scripts setup'
 # ask for super user
@@ -30,6 +51,6 @@ utils::exec_cmd 'install_prerequisites' 'Install prerequisites'
 # Give execute permissions to the scripts
 utils::exec_cmd 'give_permission' 'Give permission to scripts'
 # Create softlinks to /usr/local/bin which is usually already in the PATH
-utils::exec_cmd 'create_softlinks' 'Create soft links in /usr/local/bin'
+utils::exec_cmd 'create_softlinks' "Create soft links in "${target}""
 # Done
 echo 'Setup finished!'
